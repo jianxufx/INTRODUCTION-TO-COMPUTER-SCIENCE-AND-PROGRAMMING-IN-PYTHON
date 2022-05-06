65579536d9dd37efd740e3c902eb64ce6a461931
jianxufx <terrywang361@gmail.com>
2022-05-06 18:21:03 +0800

    Create guess_squre_root.py

---
+++ guess_squre_root.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 17:20:06 2022
+
+@author: jianxuFX
+"""
+
+
+#get input
+
+num=float(input("please enter a number:"))
+if(num<0):
+    print('negetive numbers are not allowed')
+    exit()
+
+
+g=float(input('please enter a nuber to start to guess:'))
+if g<0:
+    print('negetive numbers are not allowed')
+    exit()
+
+#close  enough 
+while abs(g*g-num)>=0.01:
+    g=(g+num/g)/2
+
+print(str(round(g,3))+' '+'is the squre root of '+' '+str(num))
+
