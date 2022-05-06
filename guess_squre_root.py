e4ba188d5b6507f1e12a5735791d93abd14c2589
jianxufx <terrywang361@gmail.com>
2022-05-06 18:25:14 +0800

    Update guess_squre_root.py

--- guess_squre_root.py
+++ guess_squre_root.py
@@ -19,9 +19,10 @@ if g<0:
     print('negetive numbers are not allowed')
     exit()
 
-#close  enough 
+#the two nubers are not close enough 
 while abs(g*g-num)>=0.01:
     g=(g+num/g)/2
 
+#find the close enough number
 print(str(round(g,3))+' '+'is the squre root of '+' '+str(num))
 
